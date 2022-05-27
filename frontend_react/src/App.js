4b9d253b4ef458dc07750815dce45bb6cd81aae9
hossem7 <mohammad.hossein0999@gmail.com>
2022-05-26 22:28:04 -0400

    Created Navbar and added dependencies

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import { About, Footer, Header, Skills, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Footer />
+    </div>
   );
 }
 
